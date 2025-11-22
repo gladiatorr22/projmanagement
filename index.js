050cb4c4f3599cd407c0a137043b86075c75c486
gladiatorr22 <likhitnataraj@gmail.com>
2025-11-22 11:30:27 +0530

    add .env files

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("start of backend project");
+import dotenv from "dotenv"
+
+dotenv.config({
+    path:"./.env",
+})
+
+let myname = process.env.database
+console.log(myname);
+
+console.log("start of a project");
