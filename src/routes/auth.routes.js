7ccb8bfddebaec07c921e79287ef0203af4433ed
gladiatorr22 <likhitnataraj@gmail.com>
2025-12-01 20:25:49 +0530

    updated route fro logout

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,9 +4,12 @@ import { validate } from "../middlewares/validator.middleware.js";
 import { userRegisterValidator } from "../validators/index.js";
 import { login } from "../controllers/auth.controllers.js";
 import { userLoginValidator } from "../validators/index.js";
+import { logoutUser } from "../controllers/auth.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(),validate,login);
+router.route("/logout").post(verifyJWT,logoutUser);
 
 export default router;
